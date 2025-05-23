ee251f557e0823535d8a0948021ca40ec032cae6
João Paulo Decker Oleinik <joaopaulodecker@gmail.com>
2025-05-23 13:19:46 -0300

    ajuste problema ID filme

--- Entidades/IndFilme.py
+++ Entidades/IndFilme.py
@@ -8,7 +8,7 @@ class IndFilme(IndicacaoAbstract):
             id_indicacao=id_indicacao,
             membro_id=membro_id,
             categoria=categoria,
-            item_indicado_id=filme_indicado.id,
+            item_indicado_id=filme_indicado.id_filme,
             tipo_item_indicado="filme"
         )
         if not isinstance(filme_indicado, Filme):
@@ -25,10 +25,8 @@ class IndFilme(IndicacaoAbstract):
             raise TypeError("item_indicado deve ser uma instância da classe Filme.")
         self.__item_indicado = filme_indicado
 
-
     def obter_detalhes_item_indicado(self) -> str:
         if self.item_indicado:
             return f"Filme: {self.item_indicado.titulo} (Ano: {self.item_indicado.ano})"
         return "Filme não especificado."
-
 
